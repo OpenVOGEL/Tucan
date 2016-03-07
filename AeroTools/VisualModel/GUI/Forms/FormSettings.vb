0077c57808c9fbdf8dd2956c68135f83f57aa493
Guillermo Hazebrouck <guillermo.hazebrouck@outlook.be>
2016-03-07 09:12:03 +0100

    GNU notice corrected

--- AeroTools/VisualModel/GUI/Forms/FormSettings.vb
+++ AeroTools/VisualModel/GUI/Forms/FormSettings.vb
@@ -1,4 +1,19 @@
-﻿'Copyright (C) 2016 Guillermo Hazebrouck
+﻿'## Open VOGEL ##
+'Open source software for aerodynamics
+'Copyright (C) 2016 Guillermo Hazebrouck (gahazebrouck@gmail.com)
+
+'This program Is free software: you can redistribute it And/Or modify
+'it under the terms Of the GNU General Public License As published by
+'the Free Software Foundation, either version 3 Of the License, Or
+'(at your option) any later version.
+
+'This program Is distributed In the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty Of
+'MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License For more details.
+
+'You should have received a copy Of the GNU General Public License
+'along with this program.  If Not, see < http:  //www.gnu.org/licenses/>.
 
 Imports AeroTools.UVLM.Settings
 
--- Vogel/Dialogos/FormAbout.vb
+++ Vogel/Dialogos/FormAbout.vb
@@ -1,4 +1,21 @@
-﻿Public NotInheritable Class AboutDialog
+﻿'## Open VOGEL ##
+'Open source software for aerodynamics
+'Copyright (C) 2016 Guillermo Hazebrouck (gahazebrouck@gmail.com)
+
+'This program Is free software: you can redistribute it And/Or modify
+'it under the terms Of the GNU General Public License As published by
+'the Free Software Foundation, either version 3 Of the License, Or
+'(at your option) any later version.
+
+'This program Is distributed In the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty Of
+'MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License For more details.
+
+'You should have received a copy Of the GNU General Public License
+'along with this program.  If Not, see < http:  //www.gnu.org/licenses/>.
+
+Public NotInheritable Class AboutDialog
 
     Private Sub AboutBox1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         ' Establezca el título del formulario.
