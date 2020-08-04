6522e7087d4c8310c7ee2610d9ed5d2ab86d2c6b
Guillermo Hazebrouck <guillermo.hazebrouck@outlook.be>
2020-08-04 07:43:39 +0200

    Missing file

---
+++ OpenVOGEL.Tucan/GraphicInterface/Forms/FormAttitude.vb
@@ -0,0 +1,52 @@
+﻿Imports OpenVOGEL.IndicationTools.Indicators
+
+Public Class FormAttitude
+
+    Public ReadOnly Property AttitudeIndicator As AttitudeIndicator
+
+    Public Sub New()
+
+        InitializeComponent()
+
+        DoubleBuffered = True
+
+        _AttitudeIndicator = New AttitudeIndicator
+
+        Dim Area As Rectangle
+        Area.X = 0
+        Area.Y = 0
+        Area.Width = Width
+        Area.Height = Height
+
+        _AttitudeIndicator.Region = Area
+
+    End Sub
+
+    Private Sub FormAttitude_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+
+        If DesignTools.DataStore.Results.ActiveFrame IsNot Nothing Then
+
+            Dim Frame = DesignTools.DataStore.Results.ActiveFrame
+
+            'NOTE: the angles are reversed because the verctors are reverted
+            ' I points to the rear
+            ' J points to the right wingtip
+            '--------------------------------------------------------------------
+
+            _AttitudeIndicator.Pitch = -Frame.Orientation.R2
+            _AttitudeIndicator.Roll = -Frame.Orientation.R3
+            _AttitudeIndicator.AirSpeed = Frame.StreamVelocity.EuclideanNorm
+            _AttitudeIndicator.Slide = Frame.StreamVelocity.Y
+            _AttitudeIndicator.Altitude = Frame.Position.Z
+
+        End If
+
+        If Visible Then
+
+            _AttitudeIndicator.DrawControl(e.Graphics)
+
+        End If
+
+    End Sub
+
+End Class
